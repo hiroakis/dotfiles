7a4a94f327aa8010fab2988e62658f1223001606
Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
2013-07-17 13:38:44 +0900

    add vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-cd ~/.vim/autoload
+DOTFILES_DIR=$(cd $(dirname $0);pwd)
+USER_DIR=~
+
+cp -p ${DOTFILES_DIR}/.vimrc ${USER_DIR}/
+
+cd ${USER_DIR}/.vim/autoload
 git clone https://github.com/tpope/vim-pathogen.git
-cd ~/.vim/bundle
+
+cd ${USER_DIR}/.vim/bundle
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/thinca/vim-quickrun.git
 git clone https://github.com/Shougo/unite.vim.git
 git clone https://github.com/vim-scripts/grep.vim.git
 git clone https://github.com/scrooloose/nerdcommenter.git
+
