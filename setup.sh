bd037ac9ed9adab650e671ede5c1d4888dc65960
Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
2013-07-17 13:30:53 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ~/.vim/autoload
+git clone https://github.com/tpope/vim-pathogen.git
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/thinca/vim-quickrun.git
+git clone https://github.com/Shougo/unite.vim.git
+git clone https://github.com/vim-scripts/grep.vim.git
+git clone https://github.com/scrooloose/nerdcommenter.git
