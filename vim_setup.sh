42822225d1d59b60e1975d24a19954dd7bfc135c
Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
2014-02-19 19:40:13 +0900

    add go syntax settings

--- vim_setup.sh
+++ vim_setup.sh
@@ -9,3 +9,9 @@ mkdir -p ${USER_DIR}/.vim/bundle
 cd ${USER_DIR}/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim
 
+#mkdir -p $HOME/.vim/ftdetect
+#mkdir -p $HOME/.vim/syntax
+#mkdir -p $HOME/.vim/autoload/go
+#ln -s /usr/local/Cellar/go/1.2/libexec/misc/vim/ftdetect/gofiletype.vim $HOME/.vim/ftdetect/
+#ln -s /usr/local/Cellar/go/1.2/libexec/misc/vim/syntax/go.vim $HOME/.vim/syntax/
+#ln -s /usr/local/Cellar/go/1.2/libexec/misc/vim/autoload/go/complete.vim $HOME/.vim/autoload/go
